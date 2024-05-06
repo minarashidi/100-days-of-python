f9b3ad1329e1d3d58f2b491dc2a9cb9a593195ce
“minadev” <mina.rashidi.86@gmail.com>
2024-05-06 22:13:37 +0200

    Add exception handling for NATO phonetic alphabet project.

--- day29-list-comprehension-NETO-alphabet/nato-project/main.py
+++ day29-list-comprehension-NETO-alphabet/nato-project/main.py
@@ -1,9 +1,19 @@
 import pandas as pd
 
 nato_data = pd.read_csv("nato_phonetic_alphabet.csv")
-letter_code_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
-# print(letter_code_dict)
+phonetic_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
+print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
-result = [letter_code_dict[letter] for letter in word]
-print(result)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        result = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, you need to enter only letters in the alphabet")
+        generate_phonetic()
+    else:
+        print(result)
+
+
+print(generate_phonetic())
